c2dbe1d417aa6df572c53ee152909945fd3cbae1
Ido Bakimer <idobakimer@gmail.com>
2025-02-24 13:22:41 +0200

    footer+responsivness for fonts

--- weiz.js
+++ weiz.js
@@ -1,5 +1,5 @@
 // When i first wrote this, it was just a single function to toggle the display of tabs, using the tabs-toggle-button.
-// but i ran into an edge case where if ive hidden the tab section, and then i resize the screen,
+// but i ran into an edge case where, if ive hidden the tab section, and then i resize the screen over 1200px,
 //  the tabs-toggle-button would be hidden, locking the user from opening the tabs.
 
 // so my solution was to add a second function to toggle the display of the tabs based on the screen size.
